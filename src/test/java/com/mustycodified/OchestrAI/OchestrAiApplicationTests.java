47971ada68b2f61380feccc486ee8001a5c1d1b4
Mustapha Musa <mustapha.musa@decagon.dev>
2026-03-12 00:12:01 +0100

    init commit

---
+++ src/test/java/com/mustycodified/OchestrAI/OchestrAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustycodified.OchestrAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OchestrAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
